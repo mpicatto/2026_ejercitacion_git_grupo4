21bf1a2c63f251b801b6271866dd8970392deb60
EmiCioNacho <ignaciorodrizgue@gmail.com>
2026-03-26 14:15:49 -0300

    Guardo inventario

---
+++ data_manager.py
@@ -0,0 +1,23 @@
+# Alumno 1 - Arquitecto de Datos
+
+inventario = []
+
+def inicializar_datos():
+    global inventario
+    inventario = [
+        {
+            "especie": "Jacarandá",
+            "estado": "Saludable",
+            "ubicacion": "Plaza San Martín"
+        },
+        {
+            "especie": "Lapacho",
+            "estado": "Enfermo",
+            "ubicacion": "Av. Sarmiento"
+        },
+        {
+            "especie": "Ceibo",
+            "estado": "Saludable",
+            "ubicacion": "Costanera"
+        }
+    ]
